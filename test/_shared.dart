fbd35146b7fe2768ee74c6bf419d0bcd34787bbc
Dmytro Glynskyi <dmytro.glynskyi@gmail.com>
2019-07-29 23:55:48 +0300

    added the dotenv library to provide the environment variables

--- test/_shared.dart
+++ test/_shared.dart
@@ -1,10 +1,11 @@
 import 'package:ews/Core/ExchangeService.dart';
 import 'package:ews/Enumerations/ExchangeVersion.dart';
 import 'package:ews/Http/WebCredentials.dart';
+import 'package:dotenv/dotenv.dart' show env;
 
 ExchangeService prepareExchangeService() {
-  final userName = String.fromEnvironment("USER_NAME");
-  final userPassword = String.fromEnvironment("USER_PASSWORD");
+  final userName = env["USER_NAME"];
+  final userPassword = env["USER_PASSWORD"];
   ExchangeService exchangeService = ExchangeService.withVersion(ExchangeVersion.Exchange2007_SP1);
   exchangeService.url = Uri.parse("https://outlook.office365.com/ews/exchange.asmx");
   exchangeService.credentials = WebCredentials(userName, userPassword, null);
