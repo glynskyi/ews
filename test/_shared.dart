8587324064b765e5b11c4397d237376715a2cc03
Dmytro Glynskyi <dmytro.glynskyi@gmail.com>
2019-07-29 23:47:45 +0300

    added the Travis secure environment variables

--- test/_shared.dart
+++ test/_shared.dart
@@ -3,8 +3,10 @@ import 'package:ews/Enumerations/ExchangeVersion.dart';
 import 'package:ews/Http/WebCredentials.dart';
 
 ExchangeService prepareExchangeService() {
+  final userName = String.fromEnvironment("USER_NAME");
+  final userPassword = String.fromEnvironment("USER_PASSWORD");
   ExchangeService exchangeService = ExchangeService.withVersion(ExchangeVersion.Exchange2007_SP1);
   exchangeService.url = Uri.parse("https://outlook.office365.com/ews/exchange.asmx");
-  exchangeService.credentials = WebCredentials("*** EMAIL ****", "*** PASSWORD ***", null);
+  exchangeService.credentials = WebCredentials(userName, userPassword, null);
   return exchangeService;
 }
--- test/travis_test.dart
+++ test/travis_test.dart
@@ -1,6 +1,11 @@
+import 'package:ews/ews.dart';
 import 'package:test/test.dart';
 
+import '_shared.dart';
+
 void main() {
-  test('travis', () {
+  test('travis', () async {
+    final service = prepareExchangeService();
+    await Folder.BindWithWellKnownFolder(service, WellKnownFolderName.Notes);
   });
 }
