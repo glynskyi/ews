8cd16d94f007c7c85ee5a7b868e65ee00a250d7b
Dmytro Glynskyi <featzima@gmail.com>
2021-01-19 11:35:18 +0200

    tests the Travis variables

--- test/test_all.dart
+++ test/test_all.dart
@@ -1,21 +1,24 @@
+import 'package:dotenv/dotenv.dart' show env;
 import 'package:test/test.dart';
 
+import 'test_appointment_crud.dart' as test_appointment_crud;
+import 'test_attachments.dart' as test_attachments;
+import 'test_auth.dart' as test_auth;
+import 'test_autodiscovery.dart' as test_autodiscovery;
+import 'test_calendar_folder.dart' as test_calendar_folder;
+import 'test_contacts.dart' as test_contacts;
+import 'test_effective_rights.dart' as test_effective_rights;
+import 'test_extended_properties.dart' as test_extended_properties;
 import 'test_folder_crud.dart' as test_folder_crud;
 import 'test_folder_hierarhy.dart' as test_folder_hierarhy;
-import 'test_calendar_folder.dart' as test_calendar_folder;
 import 'test_folder_permissions.dart' as test_folder_permissions;
-import 'test_trace_logs.dart' as test_trace_logs;
 import 'test_item_crud.dart' as test_item_crud;
-import 'test_extended_properties.dart' as test_extended_properties;
-import 'test_appointment_crud.dart' as test_appointment_crud;
-import 'test_auth.dart' as test_auth;
-import 'test_contacts.dart' as test_contacts;
-import 'test_attachments.dart' as test_attachments;
-import 'test_autodiscovery.dart' as test_autodiscovery;
 import 'test_timespan.dart' as test_timespan;
-import 'test_effective_rights.dart' as test_effective_rights;
+import 'test_trace_logs.dart' as test_trace_logs;
 
 void main() {
+  print("TEST_VARI");
+  print("TEST_VARI = ${env["TEST_VARI"]}");
   group("folder crud", test_folder_crud.main);
   group("folder hierarhy", test_folder_hierarhy.main);
   group("calendar folder", test_calendar_folder.main);
